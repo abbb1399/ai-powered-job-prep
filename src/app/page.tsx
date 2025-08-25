b773c87166b1b862b3ce0ea2e935396245542da3
paul <abbb123@naver.com>
2025-08-25 22:13:28 +0900

    theme setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { SignInButton, UserButton } from "@clerk/nextjs";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <>
       <SignInButton />
       <UserButton />
+      <ThemeToggle />
+      <Button>Click me</Button>
     </>
   );
 }
