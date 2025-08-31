a3d76a266a6c3344c079da881550d469459e52b9
paul <abbb123@naver.com>
2025-08-31 12:55:39 +0900

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { ThemeToggle } from "@/components/ThemeToggle";
-import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
@@ -8,7 +7,6 @@ export default function HomePage() {
       <SignInButton />
       <UserButton />
       <ThemeToggle />
-      <Button>Click me</Button>
     </>
   );
 }
