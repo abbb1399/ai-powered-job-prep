a65978430a37ff68ffa82a6d58e1517e309c76f2
paul <abbb123@naver.com>
2025-10-10 20:48:46 +0900

    Interview New Page

--- src/app/app/job-infos/[jobInfoId]/interviews/new/_StartCall.tsx
+++ src/app/app/job-infos/[jobInfoId]/interviews/new/_StartCall.tsx
@@ -3,8 +3,17 @@
 import { Button } from "@/components/ui/button";
 import { env } from "@/data/env/client";
 import { JobInfoTable } from "@/drizzle/schema";
+import {
+  createInterview,
+  updateInterview,
+} from "@/features/interviews/actions";
+import { errorToast } from "@/lib/errorToast";
+import { CondensedMessages } from "@/services/hume/components/CondensedMessages";
+import { condenseChatMessages } from "@/services/hume/lib/condenseChatMessages";
 import { useVoice, VoiceReadyState } from "@humeai/voice-react";
-import { Loader2, MicIcon, MicOffIcon } from "lucide-react";
+import { Loader2Icon, MicIcon, MicOffIcon, PhoneOffIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 export function StartCall({
   jobInfo,
@@ -21,14 +30,58 @@ export function StartCall({
     imageUrl: string;
   };
 }) {
-  const { connect, disconnect, readyState } = useVoice();
+  const { connect, readyState, chatMetadata, callDurationTimestamp } =
+    useVoice();
+  const [interviewId, setInterviewId] = useState<string | null>(null);
+  const durationRef = useRef(callDurationTimestamp);
+  const router = useRouter();
+  durationRef.current = callDurationTimestamp;
+
+  // Sync chat ID
+  useEffect(() => {
+    if (chatMetadata?.chatId == null || interviewId == null) {
+      return;
+    }
+    updateInterview(interviewId, { humeChatId: chatMetadata.chatId });
+  }, [chatMetadata?.chatId, interviewId]);
+
+  // Sync duration
+  useEffect(() => {
+    if (interviewId == null) return;
+    const intervalId = setInterval(() => {
+      if (durationRef.current == null) return;
+
+      updateInterview(interviewId, { duration: durationRef.current });
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, [interviewId]);
+
+  // Handle disconnect
+  useEffect(() => {
+    if (readyState !== VoiceReadyState.CLOSED) return;
+    if (interviewId == null) {
+      return router.push(`/app/job-infos/${jobInfo.id}/interviews`);
+    }
+
+    if (durationRef.current != null) {
+      updateInterview(interviewId, { duration: durationRef.current });
+    }
+    router.push(`/app/job-infos/${jobInfo.id}/interviews/${interviewId}`);
+  }, [interviewId, readyState, router, jobInfo.id]);
 
   if (readyState === VoiceReadyState.IDLE) {
     return (
       <div className="flex justify-center items-center h-screen-header">
         <Button
           size="lg"
-          onClick={async () =>
+          onClick={async () => {
+            const res = await createInterview({ jobInfoId: jobInfo.id });
+            if (res.error) {
+              return errorToast(res.message);
+            }
+            setInterviewId(res.id);
+
             connect({
               auth: { type: "accessToken", value: accessToken },
               configId: env.NEXT_PUBLIC_HUME_CONFIG_ID,
@@ -41,8 +94,8 @@ export function StartCall({
                   experienceLevel: jobInfo.experienceLevel,
                 },
               },
-            })
-          }
+            });
+          }}
         >
           Start Interview
         </Button>
@@ -56,14 +109,14 @@ export function StartCall({
   ) {
     return (
       <div className="h-screen-header flex items-center justify-center">
-        <Loader2 className="animate-spin size-24" />
+        <Loader2Icon className="animate-spin size-24" />
       </div>
     );
   }
 
   return (
     <div className="overflow-y-auto h-screen-header flex flex-col-reverse">
-      <div className="container py-6 flex flex-col items-center justify-end">
+      <div className="container py-6 flex flex-col items-center justify-end gap-4">
         <Messages user={user} />
         <Controls />
       </div>
@@ -72,7 +125,20 @@ export function StartCall({
 }
 
 function Messages({ user }: { user: { name: string; imageUrl: string } }) {
-  return <div>Messages</div>;
+  const { messages, fft } = useVoice();
+
+  const condensedMessages = useMemo(() => {
+    return condenseChatMessages(messages);
+  }, [messages]);
+
+  return (
+    <CondensedMessages
+      messages={condensedMessages}
+      user={user}
+      maxFft={Math.max(...fft)}
+      className="max-w-5xl"
+    />
+  );
 }
 
 function Controls() {
@@ -80,7 +146,7 @@ function Controls() {
     useVoice();
 
   return (
-    <div className="flex gap-5 rounded border px-5 py-2 w-fit sticky bottom-6 bg-background itmes-center">
+    <div className="flex gap-5 rounded border px-5 py-2 w-fit sticky bottom-6 bg-background items-center">
       <Button
         variant="ghost"
         size="icon"
@@ -88,9 +154,9 @@ function Controls() {
         onClick={() => (isMuted ? unmute() : mute())}
       >
         {isMuted ? <MicOffIcon className="text-destructive" /> : <MicIcon />}
-        <span className="sr-only">{isMuted ? "Unmute" : "Mute"} </span>
+        <span className="sr-only">{isMuted ? "Unmute" : "Mute"}</span>
       </Button>
-      <div className="slef-stretch">
+      <div className="self-stretch">
         <FftVisualizer fft={micFft} />
       </div>
       <div className="text-sm text-muted-foreground tabular-nums">
@@ -101,11 +167,27 @@ function Controls() {
         size="icon"
         className="-mx-3"
         onClick={disconnect}
-      ></Button>
+      >
+        <PhoneOffIcon className="text-destructive" />
+        <span className="sr-only">End Call</span>
+      </Button>
     </div>
   );
 }
 
 function FftVisualizer({ fft }: { fft: number[] }) {
-  return <div>{fft.length}</div>;
+  return (
+    <div className="flex gap-1 items-center h-full">
+      {fft.map((value, index) => {
+        const percent = (value / 4) * 100;
+        return (
+          <div
+            key={index}
+            className="min-h-0.5 bg-primary/75 w-0.5 rounded"
+            style={{ height: `${percent < 10 ? 0 : percent}%` }}
+          />
+        );
+      })}
+    </div>
+  );
 }
---
+++ src/features/interviews/actions.ts
@@ -0,0 +1,94 @@
+"use server";
+
+import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
+import { db } from "@/drizzle/db";
+import { InterviewTable, JobInfoTable } from "@/drizzle/schema";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { getJobInfoIdTag } from "@/features/jobInfos/dbCache";
+import { and, eq } from "drizzle-orm";
+import { insertInterview, updateInterview as updateInterviewDb } from "./db";
+import { getInterviewIdTag } from "./dbCache";
+
+export async function createInterview({
+  jobInfoId,
+}: {
+  jobInfoId: string;
+}): Promise<{ error: true; message: string } | { error: false; id: string }> {
+  const { userId } = await getCurrentUser();
+
+  if (userId == null) {
+    return {
+      error: true,
+      message: "You dont't have permission to do this",
+    };
+  }
+
+  const jobInfo = await getJobInfo(jobInfoId, userId);
+  if (jobInfo == null) {
+    return {
+      error: true,
+      message: "You dont't have permission to do this",
+    };
+  }
+
+  const interview = await insertInterview({
+    jobInfoId,
+    duration: "00:00:00",
+  });
+
+  return { error: false, id: interview.id };
+}
+
+export async function updateInterview(
+  id: string,
+  data: {
+    humeChatId?: string;
+    duration?: string;
+  }
+) {
+  const { userId } = await getCurrentUser();
+  if (userId == null) {
+    return {
+      error: true,
+      message: "You dont't have permission to do this",
+    };
+  }
+
+  const interview = await getInterview(id, userId);
+  if (interview == null) {
+    return {
+      error: true,
+      message: "You dont't have permission to do this",
+    };
+  }
+
+  await updateInterviewDb(id, data);
+
+  return { error: false };
+}
+
+async function getJobInfo(id: string, userId: string) {
+  "use cache";
+  cacheTag(getJobInfoIdTag(id));
+
+  return db.query.JobInfoTable.findFirst({
+    where: and(eq(JobInfoTable.id, id), eq(JobInfoTable.userId, userId)),
+  });
+}
+
+async function getInterview(id: string, userId: string) {
+  "use cache";
+  cacheTag(getInterviewIdTag(id));
+
+  const interview = await db.query.InterviewTable.findFirst({
+    where: eq(InterviewTable.id, id),
+    with: { jobInfo: { columns: { id: true, userId: true } } },
+  });
+
+  if (interview == null) return null;
+
+  cacheTag(getJobInfoIdTag(interview.jobInfo.id));
+  if (interview.jobInfo.userId !== userId) return null;
+
+  return interview;
+}
